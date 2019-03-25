447e05c637c42186c06c32d0e51428dddeeb3477
Martin Margonari <martinmargonari@gmail.com>
2019-03-25 16:47:11 -0300

    Ejercicio 1.2

---
+++ 1-2/ej1_2_a.py
@@ -0,0 +1,17 @@
+def perimetro_rectangulo(base, altura):
+    return base*2 + altura*2
+
+def area_rectangulo(base, altura):
+    return base * altura
+
+def perimetro_circulo(radio):
+    import math
+    return math.pi * 2 * radio
+
+def area_circulo(radio):
+    import math
+    return math.pi * radio**2
+
+def volumen_esfera(radio):
+    import math
+    return math.pi * radio**3
---
+++ 1-2/ej1_2_c.py
@@ -0,0 +1,7 @@
+import ej1_2_a as modulo_rectangulo
+
+def area_rectangulo_coordenadas(x1,x2,y1,y2):
+    base = x2 - x1
+    altura = y2 - y1
+    return modulo_rectangulo.area_rectangulo(base, altura)
+
